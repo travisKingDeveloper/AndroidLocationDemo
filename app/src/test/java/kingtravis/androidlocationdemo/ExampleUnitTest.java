6639ff08aef1ebf2b7f9fa7081b037efd966dfa5
${name} <${email}>
2016-06-12 09:44:42 -0400

    Initial commit of Android Studio Project

---
+++ app/src/test/java/kingtravis/androidlocationdemo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package kingtravis.androidlocationdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
